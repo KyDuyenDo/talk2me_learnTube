2a25dc3cded51016fd286c59eb51371f9183be61
Duyen <duyenk200@gmail.com>
2025-08-26 15:24:19 +0700

    feat: navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 import LoginPage from './features/auth/pages/Login'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <LoginPage />
     </>
   )
