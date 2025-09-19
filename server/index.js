18dec03ebb638a0c683bfbad2dcfe22995bfbac0
Thien879811 <119880159+Thien879811@users.noreply.github.com>
2025-09-19 14:04:33 +0700

    chore: index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(fileUpload());
 app.use(express.static("client"));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:3000",
   credentials: true
 }));
 app.use(helmet());
