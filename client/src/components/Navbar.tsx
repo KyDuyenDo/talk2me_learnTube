54b243c63b46403360042bfb2edc2646e577262f
Duyen <duyenk200@gmail.com>
2025-09-22 10:12:02 +0700

    feat: sidebar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -23,9 +23,11 @@ const Navbar: FunctionComponent = () => {
             <div className="flex bg-white flex-[0_0_auto] overflow-visible relative w-60 h-full transition-[width,left] duration-300 ease-out">
                 <div className="sticky bg-white box-content flex flex-col justify-between flex-[1_1_auto] pl-4 left-0 top-16 w-60 h-[calc(100dvh-4rem)] transition-[width, left] duration-300 ease-out">
                     <nav className="flex-[1_1_auto] mb-auto overflow-x-hidden overflow-y-auto pr-6 pt-2">
-                        {
-                            navbarItem.map((item) => <NavItem title={item.title} link={item.link} active={currentPath === item.link} />)
-                        }
+                        <div className="flex flex-col gap-4">
+                            {
+                                navbarItem.map((item) => <NavItem title={item.title} link={item.link} active={currentPath === item.link} />)
+                            }
+                        </div>
                     </nav>
                 </div>
             </div>
@@ -34,7 +36,7 @@ const Navbar: FunctionComponent = () => {
 };
 
 const NavItem: FunctionComponent<{ title: String, icon?: SVGSVGElement, link: String, active: boolean }> = ({ title, icon, link, active }) => {
-    return <div className="rounded-lg mb-1 overflow-hidden">
+    return <div className="rounded-lg overflow-hidden">
         <div className={`max-w-[12.5rem] flex rounded-lg hover:bg-gray-100 ${active ? '!bg-gray-100' : ''}`}>
             <NavLink to={link as To} className="w-full m-0 overflow-hidden rounded-lg appearance-none bg-transparent border-none cursor-pointer">
                 <div className="flex flex-col pr-4 pl-2 pt-0 pb-0 relative">
